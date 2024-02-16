04c6881e9e4012673f06cb1d80667dbe49865ab2
hadilderbali <hadil.derbali@esprit.tn>
2024-02-16 10:53:53 +0100

    backend artofcode project

---
+++ src/main/java/com/artofcode/artofcodebck/ArtOfCodeBckApplication.java
@@ -0,0 +1,13 @@
+package com.artofcode.artofcodebck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtOfCodeBckApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArtOfCodeBckApplication.class, args);
+    }
+
+}
